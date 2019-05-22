a0a4b99953f0f2425bccc709d5e0cf629c63f591
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-22 16:28:17 +0100

    Styling for the compoenent

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
     "arrow-parens": 0,
     "no-undef": 0,
     "no-new": 0,
-    "array-callback-return": 0
+    "array-callback-return": 0,
+    "no-use-before-define": 0,
+    "no-plusplus": 0
   },
 };
